4dcb6c4c67671c2846597d24a4e87f28f3e3e8af
wweiyang <nwy290@gmail.com>
2024-03-16 11:07:00 +0800

    Set up basic html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Today's Weather</h1>
+      <form>
+        <label htmlFor="city">City:</label>
+        <input type="text" id="city" placeholder="Input 1" />
+        <label htmlFor="country">Country:</label>
+        <input type="text" id="country" placeholder="Input 2" />
+        <button type="submit">Submit</button>
+        <button type="button">Clear</button>
+      </form>
+      <div>
+        <p>location placeholder</p>
+        <p>title placeholder</p>
+        <div>
+          <p>Description:</p>
+          <p>description placeholder</p>
+        </div>
+        <div>
+          <p>Temperature:</p>
+          <p>temperature placeholder</p>
+        </div>
+        <div>
+          <p>Humidity:</p>
+          <p>humidity placeholder</p>
+        </div>
+        <div>
+          <p>Time:</p>
+          <p>time placeholder</p>
+        </div>
+      </div>
+      <div>
+        <h1>Search History</h1>
+      </div>
     </div>
   );
 }
