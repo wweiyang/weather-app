e3c2078395f8ea3fdb2e05c02624e85aa19f6588
wweiyang <nwy290@gmail.com>
2024-03-17 16:57:36 +0800

    Order search history by datetime, add content for no history scenario

--- src/components/SearchHistory/History.tsx
+++ src/components/SearchHistory/History.tsx
@@ -14,6 +14,11 @@ export default function SearchHistory(): JSX.Element {
     ([key, value]) => [key, JSON.parse(value)]
   );
 
+  // sort entries by date using the keys which are the dates
+  const orderedEntries = localStorageEntries.sort(([keyA], [keyB]) => {
+    return new Date(keyB).getTime() - new Date(keyA).getTime();
+  });
+
   const handleSearch = (city: string, country: string) => {
     setCity(city);
     setCountry(country);
@@ -41,29 +46,41 @@ export default function SearchHistory(): JSX.Element {
   };
 
   return (
-    <div className={styles.historyContainer}>
-      <h1 className={styles.historyHeading}>Search History</h1>
-      {localStorageEntries.map(([key, value]) => (
-        <div className={styles.historyItem} key={key}>
-          <div className={styles.itemDetails}>
-            <p
-              className={styles.location}
-            >{`${value.location}, ${value.country}`}</p>
-            <p className={styles.datetime}>{`${value.datetime}`}</p>
-          </div>
-          <div className={styles.buttonGroup}>
-            <button
-              onClick={() => handleSearch(value.location, value.country)}
-              className={styles.button}
-            >
-              <FontAwesomeIcon icon={faSearch} />
-            </button>
-            <button onClick={() => deleteItem(key)} className={styles.button}>
-              <FontAwesomeIcon icon={faTrash} />
-            </button>
-          </div>
+    <>
+      {orderedEntries.length === 0 && (
+        <p className={styles.noHistory}>
+          Perform a search to check today's weather!
+        </p>
+      )}
+      {orderedEntries.length > 0 && (
+        <div className={styles.historyContainer}>
+          <h1 className={styles.historyHeading}>Search History</h1>
+          {orderedEntries.map(([key, value]) => (
+            <div className={styles.historyItem} key={key}>
+              <div className={styles.itemDetails}>
+                <p
+                  className={styles.location}
+                >{`${value.location}, ${value.country}`}</p>
+                <p className={styles.datetime}>{`${value.datetime}`}</p>
+              </div>
+              <div className={styles.buttonGroup}>
+                <button
+                  onClick={() => handleSearch(value.location, value.country)}
+                  className={styles.button}
+                >
+                  <FontAwesomeIcon icon={faSearch} />
+                </button>
+                <button
+                  onClick={() => deleteItem(key)}
+                  className={styles.button}
+                >
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </>
   );
 }
